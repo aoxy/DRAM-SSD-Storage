8a1f32ed475032ee24d1a3696c1e3df4f4d772fa
aoxuyang <jerryao@mail.ustc.edu.cn>
2022-01-07 12:55:14 +0800

    feat: Add LFU test

---
+++ test_init/lfu_cache.py
@@ -0,0 +1,213 @@
+import collections
+
+
+class Node:
+    def __init__(self, key, val, pre=None, nex=None, freq=0):
+        self.pre = pre
+        self.nex = nex
+        self.freq = freq
+        self.val = val
+        self.key = key
+
+    def insert(self, nex):
+        nex.pre = self
+        nex.nex = self.nex
+        self.nex.pre = nex
+        self.nex = nex
+
+
+def create_linked_list():
+    head = Node(0, 0)
+    tail = Node(0, 0)
+    head.nex = tail
+    tail.pre = head
+    return (head, tail)
+
+
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.minFreq = 0
+        self.freqMap = collections.defaultdict(create_linked_list)
+        self.keyMap = {}
+
+    def delete(self, node):
+        if node.pre:
+            node.pre.nex = node.nex
+            node.nex.pre = node.pre
+            if node.pre is self.freqMap[node.freq][0] and node.nex is self.freqMap[node.freq][-1]:
+                self.freqMap.pop(node.freq)
+        return node.key
+
+    def increase(self, node):
+        node.freq += 1
+        self.delete(node)
+        self.freqMap[node.freq][-1].pre.insert(node)
+        if node.freq == 1:
+            self.minFreq = 1
+        elif self.minFreq == node.freq - 1:
+            head, tail = self.freqMap[node.freq - 1]
+            if head.nex is tail:
+                self.minFreq = node.freq
+
+    def get(self, key: int) -> int:
+        if key in self.keyMap:
+            self.increase(self.keyMap[key])
+            return self.keyMap[key].val
+        return -1
+
+    def put_ori(self, key: int, value: int) -> None:
+        if self.capacity != 0:
+            if key in self.keyMap:
+                node = self.keyMap[key]
+                node.val = value
+            else:
+                node = Node(key, value)
+                self.keyMap[key] = node
+                self.size += 1
+            if self.size > self.capacity:
+                self.size -= 1
+                deleted = self.delete(self.freqMap[self.minFreq][0].nex)
+                self.keyMap.pop(deleted)
+            self.increase(node)
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.keyMap:
+            node = self.keyMap[key]
+            node.val = 123
+        elif self.size == self.capacity:
+            deleted = self.delete(self.freqMap[self.minFreq][0].nex)
+            self.keyMap.pop(deleted)
+            node = Node(key, 123)
+            self.keyMap[key] = node
+        else:
+            node = Node(key, 123)
+            self.keyMap[key] = node
+            self.size += 1
+        self.increase(node)
+
+
+def lfu_hit_rate(visit_list, capacity_percent, visit_dup_size):
+    visit_count = 0
+    hit_count = 0
+    miss_count = 0
+    capacity = int(capacity_percent * visit_dup_size / 100)
+    cache = LFUCache(capacity)
+    # 预先加载一些数据进内存
+    for key in visit_list:
+        if cache.size == cache.capacity:
+            break
+        if key in cache.keyMap:
+            cache.increase(cache.keyMap[key])
+        else:
+            cache.put(key, 123)
+    # 正式开始迭代
+    for key in visit_list:
+        visit_count += 1
+        if key in cache.keyMap:
+            hit_count += 1
+            cache.increase(cache.keyMap[key])
+        else:
+            miss_count += 1
+            cache.put(key, 123)
+
+    print(
+        "LFUCache[{}]({} %) {}次访问，{}次命中（{} %）".format(
+            capacity,
+            capacity_percent,
+            visit_count,
+            hit_count,
+            "%.6g" % (hit_count * 100 / visit_count),
+        )
+    )
+    return visit_count, hit_count, miss_count
+
+
+def lfu_hit_rate_first(visit_list, capacity_percent, visit_dup_size):
+    visit_count = 0
+    hit_count = 0
+    miss_count = 0
+    capacity = int(capacity_percent * visit_dup_size / 100)
+    cache = LFUCache(capacity)
+    # 预先加载一些数据进内存
+    for key in visit_list:
+        if key in cache.keyMap:
+            node = cache.keyMap[key]
+            node.val = 123
+        elif cache.size == cache.capacity:
+            deleted = cache.delete(cache.freqMap[cache.minFreq][0].nex)
+            cache.keyMap.pop(deleted)
+            node = Node(key, 123)
+            cache.keyMap[key] = node
+            cache.increase(node)
+            break
+        else:
+            node = Node(key, 123)
+            cache.keyMap[key] = node
+            cache.size += 1
+        cache.increase(node)
+
+    # 正式开始迭代
+    for key in visit_list:
+        visit_count += 1
+        if key in cache.keyMap:
+            node = cache.keyMap[key]
+            node.val = 123
+            hit_count += 1
+        elif cache.size == cache.capacity:
+            deleted = cache.delete(cache.freqMap[cache.minFreq][0].nex)
+            cache.keyMap.pop(deleted)
+            node = Node(key, 123)
+            cache.keyMap[key] = node
+            miss_count += 1
+        else:
+            node = Node(key, 123)
+            cache.keyMap[key] = node
+            cache.size += 1
+            miss_count += 1
+        cache.increase(node)
+    print(
+        "LFUCache[{}]({} %) {}次访问，{}次命中（{} %）".format(
+            capacity,
+            capacity_percent,
+            visit_count,
+            hit_count,
+            "%.6g" % (hit_count * 100 / visit_count),
+        )
+    )
+    return visit_count, hit_count, miss_count
+
+
+def main():
+    lfu = LFUCache(5)
+    print(lfu.get(12))
+    print(lfu.keyMap.keys())
+    lfu.put(12, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(14, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(13, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(15, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(16, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(12, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(14, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(19, 1)
+    print(lfu.keyMap.keys())
+    print(lfu.get(12))
+    print(lfu.keyMap.keys())
+    lfu.put(11, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(17, 1)
+    print(lfu.keyMap.keys())
+    lfu.put(12, 1)
+    print(lfu.keyMap.keys())
+
+
+if __name__ == "__main__":
+    main()
--- test_init/test_hit_rate.py
+++ test_init/test_hit_rate.py
@@ -3,7 +3,10 @@ import collections
 import csv
 import random
 import os
+import time
 import numpy as np
+
+from lfu_cache import lfu_hit_rate
 
 
 def get_ori_visit_list():
@@ -20,10 +23,10 @@ def get_ori_visit_list():
         for row in reader:
             if cnt % 1000000 == 0:
                 i = int(cnt / 2655796.2)
-                print("\r{:.2f} %".format(cnt / 265579.62), end="")
+                print("\rReading dataset: {:.2f} %".format(cnt / 265579.62), end="")
             cnt += 1
-            userid_visit_list.append(row[0])
-            adgroupid_visit_list.append(row[2])
+            userid_visit_list.append(int(row[0]))
+            adgroupid_visit_list.append(int(row[2]))
         print()
     np.save(ori_user_filepath, userid_visit_list)
     np.save(ori_ad_filepath, adgroupid_visit_list)
@@ -44,7 +47,7 @@ def gen_visit_list(epoch):
     return ori_user_list, ori_ad_list
 
 
-def hit_rate(visit_list, capacity_percent, visit_dup_size):
+def lru_hit_rate(visit_list, capacity_percent, visit_dup_size):
     visit_count = 0
     hit_count = 0
     miss_count = 0
@@ -158,7 +161,7 @@ def plot_avg():
     plt.show()
 
 
-def run():
+def run(algo="lru"):
     userid_visit_dup_size = None
     adgroupid_visit_dup_size = None
     for i in range(5):
@@ -167,29 +170,74 @@ def run():
         if not userid_visit_dup_size or not adgroupid_visit_dup_size:
             userid_visit_dup_size = len(set(userid_visit_list))
             adgroupid_visit_dup_size = len(set(adgroupid_visit_list))
-            print(
-                "user, ad dup size =", userid_visit_dup_size, adgroupid_visit_dup_size
-            )
-        print("user")
-        hit_rate(userid_visit_list, 1, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 5, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 10, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 20, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 30, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 50, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 70, userid_visit_dup_size)
-        hit_rate(userid_visit_list, 100, userid_visit_dup_size)
-        print("adgroup")
-        hit_rate(adgroupid_visit_list, 1, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 5, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 10, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 20, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 30, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 50, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 70, adgroupid_visit_dup_size)
-        hit_rate(adgroupid_visit_list, 100, adgroupid_visit_dup_size)
+            print("user dup size =", userid_visit_dup_size)
+            print("ad dup size =", adgroupid_visit_dup_size)
+
+        if algo == "lru":
+            print("[LRU]user")
+            lru_hit_rate(userid_visit_list, 1, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 5, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 10, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 20, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 30, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 50, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 70, userid_visit_dup_size)
+            lru_hit_rate(userid_visit_list, 100, userid_visit_dup_size)
+            print("[LRU]adgroup")
+            lru_hit_rate(adgroupid_visit_list, 1, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 5, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 10, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 20, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 30, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 50, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 70, adgroupid_visit_dup_size)
+            lru_hit_rate(adgroupid_visit_list, 100, adgroupid_visit_dup_size)
+        elif algo == "lfu":
+            print("[LFU]user")
+            lfu_hit_rate(userid_visit_list, 1, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 5, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 10, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 20, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 30, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 50, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 70, userid_visit_dup_size)
+            lfu_hit_rate(userid_visit_list, 100, userid_visit_dup_size)
+            print("[LFU]adgroup")
+            lfu_hit_rate(adgroupid_visit_list, 1, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 5, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 10, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 20, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 30, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 50, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 70, adgroupid_visit_dup_size)
+            lfu_hit_rate(adgroupid_visit_list, 100, adgroupid_visit_dup_size)
+        else:
+            pass
+
+
+def gen_file():
+    T1 = time.time()
+    run("null")
+    T2 = time.time()
+    print("gen file time: %s s" % (T2 - T1))
+    # gen file time: 297.9827558994293 s
+
+
+def test_lru():
+    T1 = time.time()
+    run("lru")
+    T2 = time.time()
+    print("run lru time: %s s" % (T2 - T1))
+
+
+def test_lfu():
+    T1 = time.time()
+    run("lfu")
+    T2 = time.time()
+    print("run lfu time: %s s" % (T2 - T1))
 
 
 if __name__ == "__main__":
-    plot_avg()
-    # run()
+    # plot_avg()
+    # gen_file()
+    test_lfu()
